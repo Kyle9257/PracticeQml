72cbce062f14735898faf63d249f9db0b47ebad9
Kyle9257 <1625162667@qq.com>
2026-02-26 13:15:47 +0800

    z

---
+++ PracticeQml/DeviceHandler.h
@@ -0,0 +1,13 @@
+#ifndef DEVICEHANDLER_H
+#define DEVICEHANDLER_H
+
+#include <QObject>
+
+class DeviceHandler :public QObject
+{
+    Q_OBJECT
+public:
+    DeviceHandler();
+};
+
+#endif // DEVICEHANDLER_H
